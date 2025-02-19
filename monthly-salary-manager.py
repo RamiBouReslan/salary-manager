6fcf810aa815806b723b79564b69fe1c74e22a3f
Rb-112594 <reboureslan@gmail.com>
2025-02-19 15:11:27 +0200

    Defined the input variables

---
+++ monthly-salary-manager.py
@@ -0,0 +1,11 @@
+month = input("Specify which month the salary is for: ")
+
+salary = int(input(f"Enter your salary for {month}: "))
+
+utilities = []
+utility = ""
+divided_percentage = []
+ 
+
+
+  
