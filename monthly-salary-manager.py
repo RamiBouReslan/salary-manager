e95ef0d3ec71c3aa8809b654cf63382a0620622b
Rb-112594 <reboureslan@gmail.com>
2025-02-19 16:11:12 +0200

    Calculated the actual budgets allocated to the utilities.

--- monthly-salary-manager.py
+++ monthly-salary-manager.py
@@ -17,5 +17,14 @@ while utility != "stop":
         divided_percentage.append(100 - sum(divided_percentage))
         break
 
+budget = []
 
-
+for i in range(len(utilities)):
+    amount = salary * divided_percentage[i]/100
+    budget.append(amount)
+    if utilities[i] != "Remainder":
+        print(f"The budget for {utilities[i]} is ${budget[i]}.")
+    else:
+        break
+print (f"The combined budget of the utilities is ${sum(budget)-amount}.")
+print (f"The remainder of your salary for this month is ${amount}.")
