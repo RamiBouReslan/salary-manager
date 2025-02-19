220dc6d37d5d77a7df2bd5ff9669fd13bbc59615
Rb-112594 <reboureslan@gmail.com>
2025-02-19 15:26:35 +0200

    Requested monthly budgeting details

--- monthly-salary-manager.py
+++ monthly-salary-manager.py
@@ -6,6 +6,16 @@ utilities = []
 utility = ""
 divided_percentage = []
  
+while utility != "stop":
+    utility = input("Enter allocated utility: ")
+    if utility != "stop":
+        percentage = int(input(f"Enter the pecentage allocated for {utility}: %"))
+        utilities.append(utility)
+        divided_percentage.append(percentage) 
+    else:
+        utilities.append ("Remainder")
+        divided_percentage.append(100 - sum(divided_percentage))
+        break
 
 
-  
+
